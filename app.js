fd3b931a0f055257434ef1d0af37495a060bcb2b
Ramanpreet Singh <meetraman16@outlook.com>
2019-07-23 21:51:02 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var db = require('monk')('localhost:27017/hatchways-back-end')
+var app = express();
+
+db.then(() => {
+  console.log('Connected correctly to mongo server')
+})
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function noCache(req, res, next) {
+  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.header("Pragma", "no-cache");
+  res.header("Expires", 0);
+  next();
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+
+
+/* Fetch all Work Orders */
+router.get('/work_orders', function(req, res, next) {
+  var db = req.db;
+  var workOrderCollection = db.get('workOrderCollection');
+
+  workOrderCollection.find({}).then((data,err) => {
+    if(err)
+      return 'Error Encountered while fetching data'
+
+    res.send(data);
+  })
+});
+
+
+/* Create a Work Order */
+router.post('/work_order', function(req, res, next) {
+  var db = req.db;
+  var workOrderDetailsBody = req.body;
+
+  var workOrderCollection = db.get('workOrderCollection');
+
+  workOrderCollection.insert(workOrderDetailsBody).then((workOrderData,err) => {
+    if(err)
+      return 'Error Encountered while removing worker data';
+
+    res.send(workOrderData);
+  })
+});
+
+/* create a Worker */
+router.post('/worker', function(req, res, next) {
+  var db = req.db;
+  var workerDetailBody = req.body;
+  var workerCollection = db.get('workerCollection');
+
+  workerCollection.insert(workerDetailBody).then((workerData,err) => {
+    if(err)
+      return 'Error Encountered while inserting worker data';
+
+    res.send({ message: "Worker inserted successfully", data: workerData});
+  })
+});
+
+/* Delete a Worker */
+router.delete('/worker', function(req, res, next) {
+  var db = req.db;
+  var workerDetailId = req.body._id;
+  var workerCollection = db.get('workerCollection');
+
+  workerCollection.remove({_id: workerDetailId}).then((workerData,err) => {
+    if(err)
+      return 'Error Encountered while removing worker data';
+
+    res.send({ message: "Worker inserted successfully", data: workerData});
+  })
+});
+
+
+/* Assign a worker to work order*/
+router.patch('/work_order/:oid/worker/:wid', function(req, res, next) {
+  var db = req.db;
+  var workerDetailId = req.param('wid');
+  var workOrderDetailId = req.param('oid');
+  var workOrderCollection = db.get('workOrderCollection');
+
+  workOrderCollection.find({_id: workOrderDetailId}).then((workOrderData,err) => {
+    if(err)
+      return 'Error Encountered while fetching work order data';
+
+    if(workOrderData.workers && workOrderData.workers.length > 5) {
+      return 'Work order already has max 5 workers, Worker limit cannot exceed 5 workers.';
+    }else if(workOrderData.workers && workOrderData.workers.indexOf(workerDetailId) > -1) { 
+      return 'Worker already assigned to specified work order';
+    }else{
+
+      workOrderCollection.update({_id: workOrderDetailId},{$push:{"workers" : workerDetailId}}).then((updateResponse,err)=> {
+        if(err)
+          return 'Error Encountered while updating record';
+
+        res.send({ message: "Worker assigned successfully", data: updateResponse});
+      })
+      
+    
+    }
+  })
+});
+
+
+/* Fetching worker orders for a specific worker with sorting order descending*/
+router.get('/work_order/worker/:wid', function(req, res, next) {
+  var db = req.db;
+  var workerDetailId = req.param('wid');
+  var workOrderCollection = db.get('workOrderCollection');
+
+  workOrderCollection.find({'workers': workerDetailId },{sort: {deadline: -1}}).then((workOrderData,err) => {
+    if(err)
+      return 'Error Encountered while fetching work order data';
+
+      res.send({ message: "Work order fetched successfully", data: workOrderData})
+  });
+})
+
+
+
+module.exports = router;
